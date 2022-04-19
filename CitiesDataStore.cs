4de67f5fc4ceb0fd1156c73da5df119b75e18254
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-04-19 07:40:45 +0200

    Added data store with data about some cities and return that on the endpoint request

--- CitiesDataStore.cs
+++ CitiesDataStore.cs
@@ -1,6 +1,36 @@
+using CityInfo.API.Controllers.Models;
+
 namespace CityInfo.API;
 
 public class CitiesDataStore
 {
-    
+    public List<CityDto> Cities { get; set; }
+
+    public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+    public CitiesDataStore()
+    {
+        Cities = new List<CityDto>()
+        {
+            new CityDto()
+            {
+                id = 1,
+                Name = "New York City",
+                Description = "The one with that big park."
+            },
+            new CityDto()
+            {
+                id = 2,
+                Name = "Antwerp",
+                Description = "The one with the cathedral that was never really finished."
+            },
+
+            new CityDto()
+            {
+                id = 3,
+                Name = "Paris",
+                Description = "The one with that big tower"
+            }
+        };
+    }
 }
--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -9,10 +9,6 @@ public class CitiesController : ControllerBase
     [HttpGet]
     public JsonResult GetCities()
     {
-        return new JsonResult(new List<object>
-        {
-            new {id = 1, Name = "New York City"},
-            new {id = 2, Name = "Antwerp"},
-        });
+        return new JsonResult(CitiesDataStore.Current.Cities);
     }
 }
